e6767032b551ed2e1841360644f5b1a23e513378
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-09 10:04:18 -0400

    Exercise 2 - Comments and Poundd Characters

---
+++ ex2.rb
@@ -0,0 +1,10 @@
+# Exercise 2 - Comments and Pound Characters
+# A comment, this is so you can read your program later
+# Anything after the # (octothorpe) is ignored by ruby
+
+puts "I could have code like this." # and this comment is ignored
+
+#You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+puts "This will run."
