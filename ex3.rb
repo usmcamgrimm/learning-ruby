c0c96119c81ecad0b24558c69be1f8227e950b26
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-09 10:42:09 -0400

    Exercise 3 - Numbers and Math

---
+++ ex3.rb
@@ -0,0 +1,31 @@
+# Exercise 3 - Numbers and Math
+
+puts "Now I will count my chickens."
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}" #(100) - ((25 * 3) % 4)
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #1 + 0 - 0 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?" #false
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5-7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}" #Result shows where the formula is
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
+
+# Rewrite with floating point numbers so it's more accurate
+puts 3.0 + 2.0 < 5.0 - 7.0 #false
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
