e1c493d25af9bb8aea9a2dae15d17d9f26fc2557
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-09 15:29:23 -0400

    Exercise 5 - More Variables and Printing

---
+++ ex5.rb
@@ -0,0 +1,37 @@
+# Exercise 5 - More Variables and Printing
+my_name= 'Adrian Grimm'
+my_age = 45
+my_height = 69 #inches
+my_weight = 240 #pounds
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "#{my_name} is #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+# Study Drills #
+# 1. Rename variables
+# 2. Change inches to centimeters and pounds to kilograms
+name= 'Adrian Grimm'
+age = 45
+height = 69 * 2.54 #inches to cm
+weight = 240 * 0.45359237 #pounds to kilograms
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "#{name} is #{height} centimeters tall."
+puts "He's #{weight} kilograms heavy."
+puts "Actually that's not too heavy"
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
