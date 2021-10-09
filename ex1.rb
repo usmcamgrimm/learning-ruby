b7517c9fa169ff4d492a2290bdb7bbae224c9711
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-09 09:36:06 -0400

    Exercise 1 - A Good First Program

---
+++ ex1.rb
@@ -0,0 +1,17 @@
+# Exercise 1 - A Good First Program
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+## Study Drills
+# Make your script print another line.
+puts "Made this on a new line"
+# Make your script print only one of the lines.
+# Comment each line to print only this
+puts "Print only this line"
+puts "This is an octothorpe: #."
+puts "# is used to comment out individual lines."
