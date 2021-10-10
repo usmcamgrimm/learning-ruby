8afc7dcdb30885dde4fb99b43166f0f674f8b67a
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-10 15:00:06 -0400

    Exercise 11 - Asking Questions

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+# Exercise 11 - Asking Questions
+
+# gets.chomp prompts for user input and removes the \n from the string
+print "How old are you? "
+age = gets.chomp
+
+print "How tall are you? "
+height = gets.chomp
+
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts " So, you're #{age} years old, #{height} tall and #{weight} heavy."
