069b49e971a55b755b052b8be6cc0915293cea80
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-18 06:23:52 -0400

    Exercise 30 - Else and If

---
+++ ex30.rb
@@ -0,0 +1,33 @@
+# Exercise 30 - Else and If
+
+people = 30
+cars = 40
+trucks =25
+
+if cars > people
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if trucks > cars
+  puts "That's too many trucks."
+elsif trucks < cars
+  puts "Maybe we could take the trucks."
+else
+  puts "We still can't decide."
+end
+
+if people > trucks
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
+
+if cars > people && trucks > people
+  puts "We are doomed!"
+elsif people > cars || people > trucks
+  puts "Rise up against the machines!"
+end
