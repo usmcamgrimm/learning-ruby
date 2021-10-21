79afddf2cb0c81a9ee13d6dd5cc747075fc1011f
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-21 06:31:00 -0400

    Starting The Odin Project full stack Ruby

---
+++ odin-ruby/basic-data-types/numbers.rb
@@ -0,0 +1,36 @@
+# Ruby has all the typical math operatoors:
+
+# Addition
+1 + 1 #=> 2
+
+# Subtraction
+2 - 1 #=> 1
+
+# Multiplication
+2 * 2 #=> 4
+
+# Division
+10 / 5 #=> 2
+
+# Exponents
+2 ** 2 #=> 4
+3 ** 4 #=> 81
+
+# Modulus (remainder)
+8 % 2 #=> 0
+10 % 4 #=> 2
+
+# Integers and Floats
+# Integers - whole numbers
+# Floats- numbers with a decimal point
+
+# When doing math with 2 integers the result will always be an integer
+17 / 5 #=> 3
+
+# For a more accurate answer, replace one with a float
+17 / 5.0 #=> 3.4
+
+# Converting Number Types
+# No rounding, decimals are just removed
+13.to_f #=> 13.0 (converts to float)
+13.6.to_i #=> 13 (converts to integer)
