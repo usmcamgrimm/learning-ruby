6a49ccd35f968f32706189e3925676943836cf04
Adrian Grimm <usmcamgrimm@gmail.com>
2021-10-22 09:45:55 -0400

    VVariables 2 ways

---
+++ fcc-ruby/variables.rb
@@ -0,0 +1,12 @@
+character_name = "Adrian"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+puts ("He really liked the name " + character_name)
+puts ("but he didn't like being " + character_age + ".")
+character_name = "Colton"
+character_age = "1"
+puts "There was once a man named #{character_name}"
+puts "he was #{character_age} years old."
+puts "He really liked the name #{character_name}"
+puts "but he didn't like being #{character_age}."
